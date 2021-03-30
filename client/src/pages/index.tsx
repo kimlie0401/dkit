73d4b96c8a2936e669081832067116767ad932b8
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2021-03-30 10:29:05 -0700

    #13 Sub Page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from "next/head";
-import { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import Link from "next/link";
 import Axios from "axios";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import useSWR from "swr";
 
 import { Post } from "../types";
 
@@ -12,27 +13,29 @@ import PostCard from "../components/PostCard";
 dayjs.extend(relativeTime);
 
 export default function Home() {
-  const [posts, setPosts] = useState<Post[]>([]);
+  const { data: posts } = useSWR("/posts");
 
-  useEffect(() => {
-    Axios.get("/posts")
-      .then((res) => setPosts(res.data))
-      .catch((err) => console.log(err));
-  }, []);
+  // const [posts, setPosts] = useState<Post[]>([]);
+
+  // useEffect(() => {
+  //   Axios.get("/posts")
+  //     .then((res) => setPosts(res.data))
+  //     .catch((err) => console.log(err));
+  // }, []);
 
   return (
-    <div className="pt-12">
+    <Fragment>
       <Head>
         <title>Dkit: the front page of the internet</title>
       </Head>
       <div className="container flex pt-4">
         <div className="w-160">
-          {posts.map((post) => (
+          {posts?.map((post) => (
             <PostCard post={post} key={post.identifier} />
           ))}
         </div>
       </div>
-    </div>
+    </Fragment>
   );
 }
 // *ServerSide Rendering*
--- src/routes/subs.ts
+++ src/routes/subs.ts
@@ -3,6 +3,7 @@ import { isEmpty } from "class-validator";
 import { getRepository } from "typeorm";
 
 import User from "../entities/User";
+import Post from "../entities/Post";
 import Sub from "../entities/Sub";
 import auth from "../middleware/auth";
 import user from "../middleware/user";
@@ -42,8 +43,32 @@ const createSub = async (req: Request, res: Response) => {
   }
 };
 
+const getSub = async (req: Request, res: Response) => {
+  const name = req.params.name;
+  try {
+    const sub = await Sub.findOneOrFail({ name });
+    const posts = await Post.find({
+      where: { sub },
+      order: { createdAt: "DESC" },
+      relations: ["comments", "votes"],
+    });
+
+    sub.posts = posts;
+
+    if (res.locals.user) {
+      sub.posts.forEach((p) => p.setUserVote(res.locals.user));
+    }
+
+    return res.json(sub);
+  } catch (err) {
+    console.log(err);
+    return res.status(404).json({ sub: "Sub not found" });
+  }
+};
+
 const router = Router();
 
 router.post("/", user, auth, createSub);
+router.get("/:name", user, getSub);
 
 export default router;
